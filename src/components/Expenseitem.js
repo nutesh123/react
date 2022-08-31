ddff13996d71a65d4d413440b4d900807e859a90
harry <nuteshgorde@gmail.com>
2022-08-31 20:54:43 +0530

    react

---
+++ src/components/Expenseitem.js
@@ -0,0 +1,12 @@
+
+
+function Expenseitem(){
+    return(<div>
+        <h2>Expense items</h2>
+        <li>Food Rs 10</li>
+        <li>Petrol Rs 100</li>
+        <li>Movies Rs 200</li>
+        </div>)
+}
+
+export default Expenseitem;
